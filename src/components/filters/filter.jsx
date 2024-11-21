80ec4a4c20db637b5afa0c756d2a82531d543550
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2024-11-21 00:06:05 -0300

    #CTL 5/31 - Adicionando h1 filtro e linha abaixo dos filtros

--- src/components/filters/filter.jsx
+++ src/components/filters/filter.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 
 function Filter({ children }) {
-    return <nav>
+    return <nav className='flex-col justify-center'>
+                <h1 className="pl-24 uppercase text-white font-bebas text-2xl">Filtros:</h1>
                 <ul className="flex space-x-3 mx-auto justify-center">
                     { children }
                 </ul>
+                <hr class="w-50 h-1 mx-auto my-4 bg-white border border-white rounded "></hr>
            </nav>;
 }
 
