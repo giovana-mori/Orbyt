997a38e7a7a821778e30ef0bb024ad769e0739d6
Arthur Servidor <86123199+ArthurServidor@users.noreply.github.com>
2024-11-26 10:23:43 -0300

    Dto, mapper, construcao do refreshtoken e possivel solucao de conflito

--- Pi3/Pi3/obj/Debug/net8.0/Pi3.AssemblyInfo.cs
+++ Pi3/Pi3/obj/Debug/net8.0/Pi3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1abfe3b83da7a16dd88e77a7e6e5c5a025e8360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa55feebd98ac7f3ae6079c7f0f406c8d6ca13d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
